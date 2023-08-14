76b759f7a54ffb3bd420ee552b60281b178bbe5e
Belén Aluminé Callegaris <belen.alumine@gmail.com>
2023-08-14 01:57:55 -0300

    cambios

---
+++ src/webapp/script.js
@@ -0,0 +1,13 @@
+//const sectionRecomendados = document.querySelector('#recomendados');
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(carda=>{
+    carda.addEventListener('click',()=>{alert('LA PUTA MADRE, Dario (its alaif)')});
+});
+
+//const search = document.querySelector('#search');
+//search.window.open("search.html", "Pequeños Juegos");
+
+let fondo = document.getElementById("body");
+fondo.style.backgroundImage = "url('img\fondo4.jpg')";
