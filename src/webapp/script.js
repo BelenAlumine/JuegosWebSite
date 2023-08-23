65b7d83baa44855e9c4fe6f7edb9c5838f34b937
Belén Aluminé Callegaris <belen.alumine@gmail.com>
2023-08-22 21:23:40 -0300

    script form

--- src/webapp/script.js
+++ src/webapp/script.js
@@ -1,13 +1,97 @@
-//const sectionRecomendados = document.querySelector('#recomendados');
+//creo obj
 
-const cards = document.querySelectorAll('.card');
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: '',
+}
 
-cards.forEach(carda=>{
-    carda.addEventListener('click',()=>{alert('LA PUTA MADRE, Dario (its alaif)')});
+//obtengo los elementos html
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
+const contador = document.createElement('P');
+
+email.disabled = true;
+mensaje.disabled = true;
+
+formulario.appendChild(contador);
+
+
+//guardar valores en atributos del objeto datos
+/**/
+nombre.addEventListener('input', function(event) {
+    datos.nombre = event.target.value;
+    console.log(datos.nombre);
+
+    const {nombre} = datos;
+
+    for(i=0; i<nombre.length; i++) {
+        if(nombre.charCodeAt(i)>47 && nombre.charCodeAt(i)<58){
+            mostrarMensaje("El nombre no puede contener números", true)
+            email.disable = true;
+            return;
+        }
+    }
+    email.disable = false;
 });
 
-//const search = document.querySelector('#search');
-//search.window.open("search.html", "Pequeños Juegos");
+email.addEventListener('input', function(event) {
+    datos.email = event.target.value;
+    mensaje.disable = false;
+});
 
-let fondo = document.getElementById("body");
-fondo.style.backgroundImage = "url('img\fondo4.jpg')";
+mensaje.addEventListener('input', function(event) {
+    datos.mensaje = event.target.value;
+
+    //destructuring
+    const {mensaje} = datos;
+
+
+    if(mensaje.length>=20){
+        mostrarMensaje("El mensaje es demasiados largo.", true);
+        return;
+    }
+
+    mostrarContador(mensaje.length, contador);
+});
+
+
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const {nombre, email, mensaje} = datos; //destructuring (descomponer el objeto en valores literales)
+
+    if (mensaje.length>=20) {
+        mostrarMensaje("El mensaje es demasiados largo.", true);
+        return;
+    }
+    if(nombre === '' || email ==='' || mensaje ===''){
+        mostrarMensaje("Alguno de los campos no se completó.", true);
+        return;
+    }
+    mostrarMensaje("Los datos se ingresaron correctamente.");
+});
+
+function mostrarMensaje(mensaje, error) {
+    const respuesta = document.createElement('P');
+    respuesta.textContent = mensaje;
+
+    if(error){
+        respuesta.classList.add('error');
+    }
+    else {
+        respuesta.classList.add('correcto');
+    }
+
+    formulario.appendChild(respuesta);
+    setTimeout(() => { 
+        respuesta.remove();
+    }, 5000);
+}
+
+function mostrarContador(contador, elemento) {
+    elemento.textContent = contador;//
+}
